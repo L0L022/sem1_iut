984f34c834426df4d2f5521c541729852cefff87
Escales Loïc <l0l022@openmailbox.org>
2016-12-03 10:18:35 +0100

    fin tp1 voyage, mais pas corrigé

--- BDD/tp1 voyage.sql
+++ BDD/tp1 voyage.sql
@@ -43,3 +43,50 @@ WHERE hotel = 'ANTIQUE'
 SELECT villedep
 FROM tp1_bd_voyage.voyage
 WHERE paysarr = 'MAROC'
+
+-- 8
+SELECT *
+FROM tp1_bd_voyage.optionv
+WHERE libelle LIKE '%VISITE%'
+
+-- 9
+SELECT datedep
+FROM tp1_bd_voyage.planning
+WHERE idv = 927 AND datedep BETWEEN '2004-06-01' AND '2004-07-30'
+ORDER BY datedep DESC
+
+-- 10
+SELECT numcl, nom, prenom
+FROM tp1_bd_voyage.client
+WHERE ville != 'PARIS' AND ville != 'MARSEILLE'
+ORDER BY numcl
+-- ou
+SELECT numcl, nom, prenom
+FROM tp1_bd_voyage.client
+EXCEPT
+SELECT numcl, nom, prenom
+FROM tp1_bd_voyage.client
+WHERE ville = 'PARIS'
+EXCEPT
+SELECT numcl, nom, prenom
+FROM tp1_bd_voyage.client
+WHERE ville = 'MARSEILLE'
+ORDER BY numcl
+-- ou
+SELECT numcl, nom, prenom
+FROM tp1_bd_voyage.client
+EXCEPT
+SELECT numcl, nom, prenom
+FROM tp1_bd_voyage.client
+WHERE ville IN ('PARIS','MARSEILLE')
+ORDER BY numcl
+-- ou
+SELECT numcl, nom, prenom
+FROM tp1_bd_voyage.client
+WHERE ville NOT IN ('PARIS','MARSEILLE')
+ORDER BY numcl
+
+-- 11
+SELECT numcl, nom
+FROM tp1_bd_voyage.client
+WHERE adresse IS NULL
