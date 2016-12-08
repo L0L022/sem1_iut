952de93a8937fe4e850f0c9895a2c6a5146d2bf3
L0L022 <L0L022@openmailbox.org>
2016-12-08 20:50:17 +0100

    corrige c++

--- cpp/eval1/ex2.cpp
+++ cpp/eval1/ex2.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 void inverse(char tab[]) {
   size_t taille = 0;
-  for (size_t i = 0; tab[i] != '\0'; i++) {
-    ++taille;
-  }
+  for (size_t i = 0; tab[i] != '\0'; i++) ++taille;
 
   for (size_t i = 0; i<(taille-1)/2; i++) {
     char tmp = tab[i];
--- cpp/eval1/ex5.cpp
+++ cpp/eval1/ex5.cpp
@@ -4,7 +4,47 @@ using namespace std;
 
 const size_t L = 5, C = 5;
 
-bool cherche_mot(char grille[][C], const char mot[]) {
+//mieux, normalement
+bool cherche_mot_dans_mot(const char mot_dans[], const char mot_quoi[]) {
+  size_t taille_mot_dans = 0, taille_mot_quoi = 0;
+  for (size_t i = 0; mot_dans[i] != '\0'; ++i) ++taille_mot_dans;
+  for (size_t i = 0; mot_quoi[i] != '\0'; ++i) ++taille_mot_quoi;
+
+  for(size_t i = 0; i<=taille_mot_dans-taille_mot_quoi; ++i) {
+    bool pareil = true;
+    for(size_t j = 0; j<taille_mot_quoi; ++j) {
+      if(mot_dans[i+j] != mot_quoi[j]) {
+        pareil = false;
+        break;
+      }
+    }
+    if(pareil) return true;
+  }
+  return false;
+}
+
+bool cherche_mot_dans_grille(const char grille[][C], const char mot[]) {
+  size_t taille_mot = 0;
+  for (size_t i = 0; mot[i] != '\0'; ++i) ++taille_mot;
+
+  //test horizontal
+  for(size_t ligne = 0; ligne < L and taille_mot<=L; ++ligne) {
+    char mot_ligne[C];
+    for(size_t i = 0; i<C; ++i) mot_ligne[i] = grille[ligne][i];
+    if(cherche_mot_dans_mot(mot_ligne, mot)) return true;
+  }
+
+  //test vertical
+  for(size_t colonne = 0; colonne < C and taille_mot<=C; ++colonne) {
+    char mot_colonne[L];
+    for(size_t i = 0; i<L; ++i) mot_colonne[i] = grille[i][colonne];
+    if(cherche_mot_dans_mot(mot_colonne, mot)) return true;
+  }
+  return false;
+}
+
+//bof bof
+bool cherche_mot_pas_beau(const char grille[][C], const char mot[]) {
   size_t taille = 0;
   for (size_t i = 0; mot[i] != '\0'; ++i) {
     ++taille;
@@ -43,6 +83,11 @@ bool cherche_mot(char grille[][C], const char mot[]) {
   }
 
   return false;
+}
+
+bool cherche_mot(const char grille[][C], const char mot[]) {
+  //return cherche_mot_pas_beau(grille, mot);
+  return cherche_mot_dans_grille(grille, mot);
 }
 
 int main() {
