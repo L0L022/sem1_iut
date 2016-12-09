40d2fe216d8e94c63bd082c5447223f9bcb7458a
L0L022 <L0L022@openmailbox.org>
2016-12-09 13:37:46 +0100

    liste bien

--- cpp/liste.cpp
+++ cpp/liste.cpp
@@ -25,31 +25,37 @@ bool estVide(const struct Liste& liste) {
   return liste.taille == 0;
 }
 
-void ajouterAuRang(struct Liste liste, const float nombre, const size_t rang) {
-  Maillon *iMaillon = liste.tete;
-  for(size_t i = 0; i<rang; ++i) iMaillon = iMaillon->suivant;
+void ajouterAuRang(struct Liste& liste, const float nombre, const size_t rang) {
+  Maillon **iMaillon = &liste.tete;
+  for(size_t i = 0; i<rang; ++i) iMaillon = &(*iMaillon)->suivant;
 
   Maillon *nvMaillon = new Maillon;
   nvMaillon->nombre = nombre;
+  nvMaillon->suivant = *iMaillon;
 
-  if(iMaillon == liste.tete) {
-    iMaillon = nvMaillon;
-  } else {
-    nvMaillon->suivant = iMaillon->suivant;
-    iMaillon->suivant = nvMaillon;
-  }
+  *iMaillon = nvMaillon;
 
-  if(nvMaillon->suivant == nullptr) liste.queue = iMaillon;
+  if(nvMaillon->suivant == nullptr) liste.queue = *iMaillon;
 
   ++liste.taille;
 }
 
+void enleverAuRang(struct Liste& liste, const size_t rang) {
+  Maillon **iMaillon = &liste.tete;
+  for(size_t i = 0; i<rang; ++i) iMaillon = &(*iMaillon)->suivant;
+
+  Maillon *tmp = *iMaillon;
+  *iMaillon = (*iMaillon)->suivant;
+  delete tmp;
+  --liste.taille;
+}
+
 void afficherListe(const struct Liste liste) {
-  cout << liste.tete << endl;
-  for(Maillon *iMaillon = liste.tete; iMaillon != liste.queue; iMaillon = iMaillon->suivant) {
-    cout << iMaillon << endl;
+  cout << "taille: " << liste.taille << endl;
+  for(Maillon *iMaillon = liste.tete; iMaillon != nullptr; iMaillon = iMaillon->suivant) {
+    cout << iMaillon->nombre << " ";
   }
-  cout << endl;
+  cout << endl << endl;
 }
 
 int main() {
@@ -57,11 +63,25 @@ int main() {
   afficherListe(liste);
   ajouterAuRang(liste, 1, 0);
   afficherListe(liste);
-  // ajouterAuRang(liste, 2, 0);
-  // ajouterAuRang(liste, 3, 0);
-  // ajouterAuRang(liste, 4, 0);
-  // ajouterAuRang(liste, 5, 0);
-  // afficherListe(liste);
+  ajouterAuRang(liste, 2, 0);
+  ajouterAuRang(liste, 3, 0);
+  ajouterAuRang(liste, 4, 0);
+  ajouterAuRang(liste, 5, 0);
+  ajouterAuRang(liste, 6, 1);
+  ajouterAuRang(liste, 7, 2);
+  ajouterAuRang(liste, 8, 7);
+  afficherListe(liste);
+  enleverAuRang(liste, 0);
+  enleverAuRang(liste, 6);
+  afficherListe(liste);
+  enleverAuRang(liste, 5);
+  enleverAuRang(liste, 4);
+  enleverAuRang(liste, 3);
+  enleverAuRang(liste, 2);
+  enleverAuRang(liste, 1);
+  afficherListe(liste);
+  enleverAuRang(liste, 0);
+  afficherListe(liste);
 
   return 0;
 }
