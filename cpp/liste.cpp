164740f4301d85f4ed3aef84b1c71969f26db5a5
L0L022 <L0L022@openmailbox.org>
2017-01-03 16:30:14 +0100

    fini liste.cpp

--- cpp/liste.cpp
+++ cpp/liste.cpp
@@ -38,6 +38,10 @@ void ajouterAuRang(struct Liste &liste, const float nombre, const size_t rang) {
   ++liste.taille;
 }
 
+void ajouterEnTete(struct Liste &liste, const float nombre) { ajouterAuRang(liste, nombre, 0); }
+
+void ajouterEnQueue(struct Liste &liste, const float nombre) { ajouterAuRang(liste, nombre, cardinalite(liste)); }
+
 void enleverAuRang(struct Liste &liste, const size_t rang) {
   Maillon **iMaillon = &liste.tete;
   for (size_t i = 0; i < rang; ++i)
@@ -49,19 +53,27 @@ void enleverAuRang(struct Liste &liste, const size_t rang) {
   --liste.taille;
 }
 
-float &avoirAuRang(struct Liste &liste, const size_t rang) {
+void suppressionEnTete(struct Liste &liste)  { enleverAuRang(liste, 0); }
+
+void suppressionEnQueue(struct Liste &liste) { enleverAuRang(liste, cardinalite(liste)-1); }
+
+float &avoirAuRang(const struct Liste &liste, const size_t rang) {
   Maillon *iMaillon = liste.tete;
   for (size_t i = 0; i < rang; ++i)
     iMaillon = iMaillon->suivant;
   return iMaillon->nombre;
 }
 
+float valeurEnTete(const struct Liste &liste) { return avoirAuRang(liste, 0); }
+
+float valaurEnQueue(const struct Liste &liste) { return avoirAuRang(liste, cardinalite(liste)); }
+
 void viderListe(struct Liste &liste) {
   while (!estVide(liste))
-    enleverAuRang(liste, 0);
+    suppressionEnTete(liste);
 }
 
-struct Liste copierListe(struct Liste &liste) {
+struct Liste copierListe(const struct Liste &liste) {
   struct Liste nvListe = creerListeVide();
   for (Maillon *iMaillon = liste.tete; iMaillon != nullptr;
        iMaillon = iMaillon->suivant) {
@@ -72,26 +84,42 @@ struct Liste copierListe(struct Liste &liste) {
 
 void afficherListe(const struct Liste liste) {
   cout << "taille: " << liste.taille << endl;
-  for (Maillon *iMaillon = liste.tete; iMaillon != nullptr;
-       iMaillon = iMaillon->suivant) {
-    cout << iMaillon->nombre << ",";
+  for (size_t i = 0; i < cardinalite(liste); i++) {
+    cout << "[" << i << "]=" << avoirAuRang(liste, i) << ", ";
   }
   cout << endl << endl;
 }
 
 int main() {
-  struct Liste liste = creerListeVide();
-  for (size_t i = 0; i < 20; i++) {
-    ajouterAuRang(liste, i, 0);
-  }
-  afficherListe(liste);
-  struct Liste liste_2 = copierListe(liste), liste_3 = liste;
-  avoirAuRang(liste, 5) = 88;
-  ajouterAuRang(liste_2, 99, 0);
-  ajouterAuRang(liste_3, 66, 0);
-  afficherListe(liste);
-  afficherListe(liste_2);
-  afficherListe(liste_3);
+  struct Liste liste1 = creerListeVide(), liste2;
+
+  ajouterEnTete(liste1, 1);
+  ajouterEnTete(liste1, 2);
+  ajouterEnTete(liste1, 3);
+  ajouterEnQueue(liste1, 4);
+  ajouterEnQueue(liste1, 5);
+  ajouterEnQueue(liste1, 6);
+  ajouterAuRang(liste1, 7, 3);
+  afficherListe(liste1);
+
+  liste2 = liste1;
+  afficherListe(liste2);
+
+  liste2 = copierListe(liste1);
+  afficherListe(liste2);
+
+  suppressionEnTete(liste1);
+  suppressionEnQueue(liste1);
+  enleverAuRang(liste1, 3);
+  afficherListe(liste1);
+  afficherListe(liste2);
+
+  viderListe(liste1);
+  afficherListe(liste1);
+  afficherListe(liste2);
+
+  viderListe(liste2);
+  afficherListe(liste2);
 
   return 0;
 }
